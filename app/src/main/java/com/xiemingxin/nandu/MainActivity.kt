8b5ec0666c7cfc4d19d98460dd76105422826490
xiemingxin1145 <gayakethanda@gmail.com>
2026-06-14 18:00:55 +0800

    fix: add missing dp import

--- app/src/main/java/com/xiemingxin/nandu/MainActivity.kt
+++ app/src/main/java/com/xiemingxin/nandu/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.xiemingxin.nandu.ui.EmperorViewModel
