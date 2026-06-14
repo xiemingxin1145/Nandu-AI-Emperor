b2b733620d355592c97934b21ee9c0bb22be9647
xiemingxin1145 <gayakethanda@gmail.com>
2026-06-14 21:19:33 +0800

    chore: bump version to V0.5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.xiemingxin.nandu"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "V0.5.4"
+        versionCode = 10
+        versionName = "V0.5.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
