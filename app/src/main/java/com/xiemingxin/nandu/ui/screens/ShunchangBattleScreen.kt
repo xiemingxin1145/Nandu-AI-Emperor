9390edba438000b9d31a5ecc3dc566995c008775
xiemingxin1145 <gayakethanda@gmail.com>
2026-08-21 12:01:59 +0800

    fix(STAB-003): use ColumnScope weight without internal import

--- app/src/main/java/com/xiemingxin/nandu/ui/screens/ShunchangBattleScreen.kt
+++ app/src/main/java/com/xiemingxin/nandu/ui/screens/ShunchangBattleScreen.kt
@@ -2,7 +2,6 @@ package com.xiemingxin.nandu.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.weight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
